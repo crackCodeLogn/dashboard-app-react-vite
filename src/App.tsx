8ae03ee43ef6ccac34f16cccb86069f9b71d8f54
Vivek <vivektheintel@gmail.com>
2025-01-14 21:22:29 -0500

    Base commit

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+// import './App.css'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import NavigationBar from "./navbar/NavigationBar.tsx";
+import Home from "./pages/Home.tsx";
+import Contact from "./pages/Contact.tsx";
+import Market from "./pages/Market.tsx";
+
+
+function App() {
+
+    return (
+        <BrowserRouter>
+            <NavigationBar/>
+            <Routes>
+                <Route path={"/"} element={<Home/>}/>
+                <Route path={"contact"} element={<Contact/>}/>
+                <Route path={"market"} element={<Market/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
+
+    /*return (
+      <>
+        <div>
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
+      </>
+    )*/
+}
+
+export default App
---
+++ src/navbar/NavigationBar.tsx
@@ -0,0 +1,26 @@
+import {Link} from "react-router-dom";
+import {useState} from "react";
+import './NavigationBar.css';
+
+const NavigationBar = () => {
+    const [menuActive, setMenuActive] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuActive(!menuActive);
+    }
+
+    return (
+        <nav className={"navbar"}>
+            <div className={`menu-toggle ${menuActive ? 'active' : ''}`} onClick={toggleMenu}>
+                ☰
+            </div>
+            <div className={`links ${menuActive ? 'active' : ''}`}>
+                <Link to={"/"}>Home</Link>
+                <Link to={"/contact"}>Contact</Link>
+                <Link to={"/market"}>Market</Link>
+            </div>
+        </nav>
+    )
+};
+
+export default NavigationBar;
