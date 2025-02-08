68c88da83964a7d40942480d64abcfa6c7d25dcf
Vivek <vivektheintel@gmail.com>
2025-02-07 23:49:19 -0500

    Incorporate chart of GIC Valuation data from twm-portfolio-service

--- src/services/GicDataService.tsx
+++ src/services/GicDataService.tsx
@@ -2,7 +2,8 @@ import axios, {AxiosInstance} from 'axios';
 
 const HOST_URL: string = import.meta.env.VITE_HOST;
 const PORTFOLIO_DATA_BASE_URL: string = `http://${HOST_URL}:40037`;
-const PORTFOLIO_GIC_DATA_END_POINT: string = "/portfolio/gic/expiries?ccy=CAD";
+const PORTFOLIO_GIC_EXPIRIES_DATA_END_POINT: string = "/portfolio/gic/expiries?ccy=CAD";
+const PORTFOLIO_GIC_VALUATIONS_DATA_END_POINT: string = "/portfolio/gic/valuations?ccy=CAD";
 
 function generateApi(timeout: number, useProto: boolean): AxiosInstance {
   return axios.create({
@@ -16,16 +17,24 @@ function generateApi(timeout: number, useProto: boolean): AxiosInstance {
   })
 }
 
-export const fetchGicData = async (timeout: number = 3000,
-                                   useProto: boolean = false) => {
-  const gicEndPoint: string = PORTFOLIO_GIC_DATA_END_POINT;
-  console.log(gicEndPoint); // todo - remove post testing
+async function fetch(timeout: number, useProto: boolean, endPoint: string) {
+  console.log(endPoint); // todo - remove post testing
   const api: AxiosInstance = generateApi(timeout, useProto);
   try {
-    const response = await api.get(gicEndPoint);
+    const response = await api.get(endPoint);
     return response.data;
   } catch (error) {
     console.error('api err: ' + error);
     throw error;
   }
+}
+
+export const fetchGicExpiriesData = async (timeout: number = 3000,
+                                           useProto: boolean = false) => {
+  return fetch(timeout, useProto, PORTFOLIO_GIC_EXPIRIES_DATA_END_POINT);
+};
+
+export const fetchGicValuationData = async (timeout: number = 3000,
+                                            useProto: boolean = false) => {
+  return fetch(timeout, useProto, PORTFOLIO_GIC_VALUATIONS_DATA_END_POINT);
 };
